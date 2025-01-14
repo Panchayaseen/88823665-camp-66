7973dfa885d1fd1dc9a4fa26301d4585111de6f0
Panchayaseen <66160363@go.buu.ac.th>
2025-01-14 19:19:09 +0700

    update login

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -2,17 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', 
+    [LoginController::class, 'index']);
 
-Route::get('/hello', function () {
-    return "<h1>Hello World!</h1>";
-});
+Route::get('/register', 
+    [RegisterController::class, 'index']);
 
-Route::get("/mycontroller/{id?}", 
+Route::get("/", function(){
+    return view("layouts.default");});  
+
+ Route::get('/home', function(){
+    return view("layouts.defualt");
+    });
+
+Route::get('/mycontroller/{id?}', 
     [MyController::class,'myfunction']);
 
- Route::post("/mycontroller/{id?}", 
-     [MyController::class,'myfunction']);
+Route::post("/mycontroller/{id?}", 
+    [MyController::class,'myfunction']);
+    
+ Route::post("/hello", function(){
+    return "<h1>Hello World!</h1>";});  
