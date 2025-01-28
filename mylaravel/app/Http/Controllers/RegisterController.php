c4d1ffb2ca229a0a15a43c97577ed7d7810e6b80
Panchayaseen <66160363@go.buu.ac.th>
2025-01-28 18:51:17 +0700

    Update user

--- mylaravel/app/Http/Controllers/RegisterController.php
+++ mylaravel/app/Http/Controllers/RegisterController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Users;
+
 class RegisterController extends Controller
 {
     //
     function index(){
         return view('register');
+    }
+
+    function create(Request $req){
+        $obj_user = new Users;
+        $obj_user->name = $req->input('name');
+        $obj_user->email = $req->input('name');
+        $obj_user->password = $req->input('name');
+        print_r($req->input());
     }
 }
