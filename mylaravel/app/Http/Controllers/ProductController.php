48349b4841ccd4bb066f9d3f109ce0bad92b90e3
Panchayaseen <66160363@go.buu.ac.th>
2025-02-25 23:30:18 +0700

    UpUp

--- mylaravel/app/Http/Controllers/ProductController.php
+++ mylaravel/app/Http/Controllers/ProductController.php
@@ -1,13 +1,21 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Models;
 
-use Illuminate\Http\Requst;
-use App\Models\Categories;
-use App\Models\ProductList;
+use Illuminate\Database\Eloquent\Model;
 
-class ProductController extends Controller
+class ProductList extends Model
 {
     //
-    function index()
+    protected $table = 'product_list';
+    public $timestamps = false;
+
+    protected $fillable = ['name', 'category_id', 'user_id'];
+
+    public function category(){
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+    public function user(){
+        return $this->belongsTo(User::class,  'user_id');
+    }
 }
--- mylaravel/resources/views/product.blade.php
+++ mylaravel/resources/views/product.blade.php
@@ -1,30 +1,70 @@
-@extends('layouts.defaultwith_menu')
-@section('content')
-<form>
-    <div class="row">
-        <div class="col-6">
-            <div class="mb-3">
-                <label for="category" class="form-label">Category Name</label>
-                <input type="text" name="category" class="form-control" id="category">
-            </div> 
-        </div> 
-    </div>    
-</form>
-<table class="table">
-    <thead>
-        <tbody>
+@extends('layouts.default_with_menu')
 
-        </tbody>
+@section('content')
+<form action="{{url ('/product')}}" method="post">
+    @csrf
+    <div class="row mt-3">
+        <div class="col-6">
+            <label>Category Name</label>
+            <input name="category_name" type="text" class="form-control" />
+        </div>
+    </div>
+    <button class="btn btn-primary mt-3" id="btn-add-product-list" type="button"> + เพิ่ม Product</button>
+    <div class="row mt-3" id="product-list">
+        <div class="col-6">
+            <label>Product Name <button type="button"
+                    class="btn btn-danger ml-3 mt-2 mb-2 btn-del-product-list">ลบ</button></label>
+            <input name="product_name[]" type="text" class="form-control" />
+        </div>
+    </div>
+    <button type="submit" class="btn btn-success mt-3 mb-3">บันทึก</button>
+</form>
+
+<div class="card-body"></div>
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <td>#</td>
+            <td>Category Name</td>
+            <td>Product List</td>
+            <td>User Name</td>
+        </tr>
     </thead>
+    <tbody>
+        @foreach($categories as $index => $category)
+        <tr class="align-middle">
+            <td>{{ $index + 1 }}.</td>
+            <td>{{ $category->name }}</td>
+            <td>
+                <ul>
+                    @foreach($category->products as $product)
+                        <li>{{ $product->name }}</li>
+                    @endforeach
+                </ul>
+            </td>
+            <td>{{ session('user')->name }}</td>
+        </tr>
+        @endforeach
+    </tbody>
 </table>
+</div>
 @endsection
 
 @section('scripts')
 <script>
     $(document).ready(function(){
-        var count = 1;
-        $('#btn-add-product').on('click', function(){
+        $('#btn-add-product-list').on('click', function(){
+            $('#product-list').append(
+                `<div class="col-6">
+                    <label>Product Name <button type="button"
+                        class="btn btn-danger ml-3 mt-2 mb-2 btn-del-product-list">ลบ</button></label>
+                    <input name="product_name[]" type="text" class="form-control" />
+                </div>`);
+        });
 
-        })
-    })
+        $(document).on('click', '.btn-del-product-list', function(){
+            $(this).closest('.col-6').remove();
+        });
+    });
 </script>
+@endsection
